1584c5d7f0ddc41a1ada618d83db4f4c7c3cd1a0
Antojljk <antoine.drutel@gmail.com>
2026-09-12 18:36:19 +0200

    chore: add google site verification meta tag

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -25,6 +25,9 @@ export const metadata: Metadata = {
   alternates: {
     canonical: '/',
   },
+  verification: {
+    google: 'gsvZFwsnoyNuosL1jbzsoI-VyxZI7iSbGbwkGLGV_ZI',
+  },
   robots: {
     index: true,
     follow: true,
